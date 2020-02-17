e8186450bac2b15e5f47ee18c5d895d0888b4f5b
Das <surojit.das@changehealthcare.com>
2020-02-17 12:07:49 -0600

    added ef change

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+namespace DatingApp.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] Passwordhash { get; set; }
+        public byte[] Passwordsalt { get; set; }
+
+
+    }
+}
